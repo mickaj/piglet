e9dcc23c406ad86251089463c2d97020ce085f37
Mic Kajzer <mickaj@mkajzer.pl>
2019-03-26 20:54:21 +0100

    added IPhoneTypeDetector interface

---
+++ TranslateLib/IPhoneTypeDetector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TranslateLib
+{
+    public interface IPhoneTypeDetector
+    {
+        PhoneType Detect(char c);
+
+        void AddVowel(char c);
+        void AddConsonant(char c);
+    }
+}
