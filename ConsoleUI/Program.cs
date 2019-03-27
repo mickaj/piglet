d648fefbc88a5966c0179770707a61c4634b92a3
Mic Kajzer <mickaj@mkajzer.pl>
2019-03-27 09:10:51 +0100

    ConsoleUI works

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using TranslateLib;
 
 namespace ConsoleUI
 {
     class Program
     {
+        static Translator translator = new Translator(new PhoneTypeDetector());
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Input string: ");
+            string input = Console.ReadLine();
+            Console.WriteLine("Output string: ");
+            Console.WriteLine(translator.Translate(input));
+            Console.ReadKey();
         }
     }
 }
