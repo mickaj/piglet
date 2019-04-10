2ac56ba4b559dc416503cb5428f2278436a8a20d
Mic Kajzer <mickaj@mkajzer.pl>
2019-04-10 08:24:57 +0200

    DiacriticsRemover added

---
+++ TranslateLib/DiacriticsRemover.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace TranslateLib
+{
+    public class DiacriticsRemover
+    {
+        public string Remove(string input)
+        {
+            string normalized = input.Normalize(NormalizationForm.FormD);
+            StringBuilder sb = new StringBuilder();
+
+            foreach(char c in normalized)
+            {
+                UnicodeCategory unicideCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if(unicideCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString().Normalize(NormalizationForm.FormC);
+        }
+    }
+}
