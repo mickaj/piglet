81cc1fff5459cd67b6cc4a2d58da0adfea3cc306
Mic Kajzer <mickaj@mkajzer.pl>
2019-04-10 08:47:34 +0200

    Test for DiacriticsRemover

---
+++ TranslateLib.Tests/DiacriticsRemoverTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TranslateLib.Tests
+{
+    public class DiacriticsRemoverTests
+    {
+        private string _sampleString = "ĄąĆćĘęŁłŃńÓóŚśŻżŹź";
+        private string _sampleResultString = "AaCcEeLlNnOoSsZzZz";
+
+        private DiacriticsRemover _remover = new DiacriticsRemover();
+
+        [Fact]
+        public void TestRemover()
+        {
+            string result = _remover.Remove(_sampleString);
+            Assert.Equal(_sampleResultString, result);
+        }
+    }
+}
