20b89017264cf59d260d053ac849e83304c36eeb
Mic Kajzer <mickaj@mkajzer.pl>
2019-04-12 09:40:57 +0200

    ajax call set up

--- WebUI/Controllers/HomeController.cs
+++ WebUI/Controllers/HomeController.cs
@@ -25,5 +25,10 @@ namespace WebUI.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Translate()
+        {
+            return Content("translated");
+        }
     }
 }
